dfcaa9f0d4e2b9c1acc3e8015d11b7955e597188
emgulliver <emilyagulliver@gmail.com>
2023-09-04 15:10:36 +0800

    why choose us

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -2,9 +2,9 @@
     <section class="section">
       <div class="container is-flex is-flex-direction-column is-align-items-center">
         <h2 class="info-heading has-text-centered">BUILD A BETTER TOMORROW</h2>
-        <p class="info-paragraph has-text-justified has-text-centered">Southstruct WA is an Indigenous, family owned and operated Concrete Construction Company. Our services are provided throughout Western Australia, with over 20 years experience working in the residential, commercial, and civil sectors. Southstruct WA provides all groundworks, foundation, reinforcement, formwork and concrete construction services.</p>
+        <p class="info-paragraph has-text-centered">Southstruct WA is an Indigenous, family owned and operated Concrete Construction Company. Our services are provided throughout Western Australia, with over 20 years experience working in the residential, commercial, and civil sectors. Southstruct WA provides all groundworks, foundation, reinforcement, formwork and concrete construction services.</p>
         <div class="info-section">
-          <div class="columns mt-5">
+          <div class="columns">
         <div class="column">
           <div class="box has-text-centered" style="width: 35vh;">
             <p class="is-size-3">18.</p>
--- src/components/Why.vue
+++ src/components/Why.vue
@@ -1,30 +1,30 @@
 <template>
-    <section class="section" style="min-height: 400px">
+    <section class="section" style="min-height: 600px">
         <div class="has-text-centered">
             <h2 class="is-size-3 has-text-weight-semibold mt-5 mb-5">
                 WHY CHOOSE US?
             </h2>
         </div>
       <div class="why-container columns is-multiline mt-5">
-        <div class="column">
+        <div class="column is-6">
           <div class="box has-text-centered is-flex is-flex-direction-column is-justify-content-space-evenly" style="min-height: 200px" >
             <h2 class="has-text-weight-semibold">EXPERTISE</h2>
             <p>20 years experience in various concrete construction sectors</p>
           </div>
         </div>
-        <div class="column">
+        <div class="column is-6">
           <div class="box has-text-centered is-flex is-flex-direction-column is-justify-content-space-evenly" style="min-height: 200px" >
             <h2 class="has-text-weight-semibold">QUALITY</h2>
             <p>Highest standard and quality assured in all works undertaken</p>
           </div>
         </div>
-        <div class="column">
+        <div class="column is-6">
           <div class="box has-text-centered is-flex is-flex-direction-column is-justify-content-space-evenly" style="min-height: 200px">
             <h2 class="has-text-weight-semibold">COLLABORATION</h2>
             <p>We work with the client to meet their needs and goals in a timely manner</p>
           </div>
         </div>
-        <div class="column">
+        <div class="column is-6">
           <div class="box has-text-centered is-flex is-flex-direction-column is-justify-content-space-evenly" style="min-height: 200px">
             <h2 class="has-text-weight-semibold">SAFETY</h2>
             <p>We ensure zero accidents and injuries for project success</p>
